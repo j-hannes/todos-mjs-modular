d64b58acca329653f0c9e36f9279b0c0b24ed05b
j-hannes <Hannes_E@gmx.de>
2015-04-12 18:42:56 +0200

    Add browserify

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,23 @@
 'use strict';
 
-var gulp = require('gulp');
-var concat = require('gulp-concat');
-var changed = require('gulp-changed');
-var connect = require('gulp-connect');
+// gulp modules
+var gulp       = require('gulp');
+var changed    = require('gulp-changed');
+var concat     = require('gulp-concat');
+var connect    = require('gulp-connect');
+var uglify     = require('gulp-uglify');
+var gutil      = require('gulp-util');
+var sourcemaps = require('gulp-sourcemaps');
 
-gulp.task('build', ['html', 'css', 'images']);
-gulp.task('dev', ['build', 'connect', 'watch']);
+// browserify specific modules
+var watchify   = require('watchify');
+var browserify = require('browserify');
+var source     = require('vinyl-source-stream');
+var buffer     = require('vinyl-buffer');
+var assign     = require('lodash.assign');
 
-// ### build
+gulp.task('build', ['html', 'css', 'images', 'browserify']);
+gulp.task('dev', ['html', 'css', 'images', 'watchify', 'connect', 'watch']);
 
 gulp.task('html', function() {
   gulp.src('./html/index.html')
@@ -29,8 +38,6 @@ gulp.task('images', function() {
     .pipe(gulp.dest('./dist/img'));
 });
 
-// ### dev
-
 gulp.task('connect', function() {
   connect.server({
     root: 'dist',
@@ -41,4 +48,41 @@ gulp.task('connect', function() {
 gulp.task('watch', function() {
   gulp.watch('./html/index.html', ['html']);
   gulp.watch('./css/**/*.css', ['css']);
+});
+
+// ### browserify
+
+var customOpts = {
+  entries: ['./client/main.js'],
+  debug: true
+};
+var opts = assign({}, watchify.args, customOpts);
+var b = browserify(opts);
+var w = watchify(b);
+
+var bundle = function() {
+  return w.bundle()
+    .on('error', gutil.log.bind(gutil, 'Browserify Error'))
+    .pipe(source('bundle.js'))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({loadMaps: true}))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('./dist/js'))
+    .pipe(connect.reload());
+};
+
+gulp.task('watchify', bundle);
+w.on('update', bundle);
+w.on('log', gutil.log);
+
+gulp.task('browserify', function() {
+  var b = browserify({
+    entries: ['./client/main.js']
+  });
+
+  return b.bundle()
+    .pipe(source('bundle.js'))
+    .pipe(buffer())
+        .pipe(uglify())
+    .pipe(gulp.dest('./dist/js/'));
 });
