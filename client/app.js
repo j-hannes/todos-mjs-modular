b984f51c61695f068cf546d61de28c1170177b95
j-hannes <Hannes_E@gmx.de>
2015-04-13 08:28:46 +0200

    Use an application layout view

--- client/app.js
+++ client/app.js
@@ -1,9 +1,12 @@
 'use strict';
 
 var Marionette = require('backbone.marionette');
+var AppView = require('./views/app-view.js');
 
 module.exports = Marionette.Application.extend({
+  rootView: new AppView(),
+
   onStart: function() {
     console.log('marionette app started');
-  }
+  },
 });
