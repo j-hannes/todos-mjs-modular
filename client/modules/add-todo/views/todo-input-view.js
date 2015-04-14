387c2927ba8f04abac7ba01e8af89719c2ccb48e
j-hannes <Hannes_E@gmx.de>
2015-04-14 19:05:34 +0200

    Use a tag name to avoid extra container

--- client/modules/add-todo/views/todo-input-view.js
+++ client/modules/add-todo/views/todo-input-view.js
@@ -2,5 +2,6 @@ var Marionette = require('backbone.marionette')
 
 // is it better to use just "View" here?
 module.exports = Marionette.ItemView.extend({
+  tagName: 'form',
   template: '#todo-input-template',
 })
