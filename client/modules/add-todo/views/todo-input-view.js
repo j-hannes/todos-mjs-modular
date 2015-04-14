71e68af3a94422f2cfc5f47f31e937c9e02c45f0
j-hannes <Hannes_E@gmx.de>
2015-04-14 19:15:01 +0200

    Handle UI event: form submit

--- client/modules/add-todo/views/todo-input-view.js
+++ client/modules/add-todo/views/todo-input-view.js
@@ -1,7 +1,20 @@
+'use strict'
+
 var Marionette = require('backbone.marionette')
 
-// is it better to use just "View" here?
 module.exports = Marionette.ItemView.extend({
   tagName: 'form',
   template: '#todo-input-template',
+
+  events: {
+    submit: 'handleFormSubmit',
+  },
+
+  handleFormSubmit: function(e) {
+    e.preventDefault()
+  },
+
+  initialize: function() {
+    this.$newTodo = this.$('#new-todo')
+  },
 })
