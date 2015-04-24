03be743a01cb2417ace1710a6ca539a1228e4c42
j-hannes <Hannes_E@gmx.de>
2015-04-24 17:49:15 +0200

    Separate private function

--- client/entities/todo/todo-collection.js
+++ client/entities/todo/todo-collection.js
@@ -5,14 +5,25 @@ var TodoModel = require('./todo-model')
 
 var appChannel = require('backbone.radio').channel('app')
 
+
+// ####################
+// ### private area ###
+// ####################
+
+//+ nodifyItemAdded :: TodoModel -> AppEvent (TodoModel)
+var notifyItemAdded = function(todo) {
+  appChannel.trigger('todo:created', todo)
+}
+
+
+// ###########
+// ### API ###
+// ###########
+
 module.exports = Backbone.Collection.extend({
   model: TodoModel,
 
   initialize: function() {
-    this.listenTo(this, 'add', this.notifyItemAdded)
-  },
-
-  notifyItemAdded: function(todo) {
-    appChannel.trigger('todo:created', todo)
+    this.listenTo(this, 'add', notifyItemAdded)
   },
 })
