9e294ac4c5f80c4ba8ad9c1c1c4f85237ac0b981
j-hannes <Hannes_E@gmx.de>
2015-05-16 14:34:05 +0200

    Revert "Inject test environment in test command"
    This reverts commit 2e0f762647ed01015e20580ec13ec4bd80c54d36.

--- bin/mocha.sh
+++ bin/mocha.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
 find client -name *.test.js \
-  | xargs node_modules/.bin/mocha \
-      --require=node_modules/mocha-clean/brief \
-      --require=client/setup/test-env
+  | xargs node_modules/.bin/mocha --require=node_modules/mocha-clean/brief
