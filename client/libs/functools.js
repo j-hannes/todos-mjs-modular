3964dfa68f2992e7850de8ff9d27c4169b35fb13
j-hannes <Hannes_E@gmx.de>
2015-05-23 12:20:33 +0200

    Add function to filter object values
    Can be improved later ... can we add a reduceObj function?

--- client/libs/functools.js
+++ client/libs/functools.js
@@ -1,3 +1,5 @@
+'use strict'
+
 /**
  * This module is a wrapper around ramda.js to expose its functions to
  * the global namespace and complememt it with Haskell standard library
@@ -15,6 +17,16 @@ r.lines = r.split('\n')
  * Rename functions that have a different name in Haskell.
  */
 r.elem = r.contains
+
+/**
+ * Add a function to filter object content.
+ */
+r.filterObj = r.curry(function(f, obj) {
+  return r.reduce(function(accObj, key) {
+    if (f(obj[key])) accObj[key] = obj[key]
+    return accObj
+  }, {}, r.keys(obj))
+})
 
 // export the modified ramda library
 module.exports = r
