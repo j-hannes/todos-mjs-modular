c60d72870790cf455d43f7d8035f143998deb02a
j-hannes <Hannes_E@gmx.de>
2015-05-28 09:53:53 +0200

    Cover todo collection

--- client/src/data/todo/todo-collection.js
+++ client/src/data/todo/todo-collection.js
@@ -23,7 +23,26 @@ var notifyItemAdded = function(todo) {
 module.exports = Backbone.Collection.extend({
   model: TodoModel,
 
+  addTodo: function(title) {
+    if (typeof title !== 'string') return
+    var trimmedTitle = title && title.trim()
+    if (!trimmedTitle) return
+    this.add({title: title})
+  },
+
+  listTodos: function() {
+    return this.toJSON()
+  },
+
   initialize: function() {
     this.listenTo(this, 'add', notifyItemAdded)
+
+    dataChannel.comply('todos:add', this.addTodo, this)
+    dataChannel.reply('todos:list', this.listTodos, this)
+  },
+
+  destroy: function() {
+    dataChannel.stopComplying(null, this.addTodo)
+    dataChannel.stopReplying(null, this.listTodos)
   },
 })
