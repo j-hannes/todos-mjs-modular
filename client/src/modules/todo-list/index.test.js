d3d0dd5bc8fa5f39748797b608b60b49fdd89ebd
j-hannes <Hannes_E@gmx.de>
2015-06-04 18:59:14 +0200

    Create naked todo-list module

---
+++ client/src/modules/todo-list/index.test.js
@@ -0,0 +1,26 @@
+'use strict'
+
+var Module = require('../../common/module')
+var TodoList = require('./')
+
+describe('TodoList :: Module', function() {
+
+  var todoList
+
+  beforeEach(function() {
+    todoList = new TodoList()
+  })
+
+  afterEach(function() {
+    todoList.destroy()
+  })
+
+  it('should be a module', function() {
+    todoList.should.be.instanceOf(Module)
+  })
+
+  it('should have autostart set to true', function() {
+    todoList.should.have.property('autostart')
+    todoList.autostart.should.be.true
+  })
+})
